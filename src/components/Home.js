e9cdd4e3ebf5d2ba56c230e668e33291b898cbe3
unknown <matheus.condini@gmail.com>
2018-04-03 17:22:09 -0300

    New Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,34 @@ import { Grid, Row, Col } from 'react-bootstrap';
 function Home (props) {
   return (
     <section id="home">
-      
+      <div className="tbl">
+        <div className="tbl-row">
+          <div className="tbl-cell v-bottom"><span className="curly">&#123;</span></div>
+
+          <div className="tbl-cell v-bottom">
+              <div className="wrap-hexagon">
+                <div className="hexagon">
+                  <div className="hexTop"></div>
+                  <div className="hexBottom"></div>
+                </div>
+              </div>
+          </div>
+
+          <div className="tbl-cell v-bottom"><span className="curly">&#125;</span></div>
+        </div>
+        <div className="tbl-row">
+
+          <div className="tbl-cell v-top"></div>
+          <div className="tbl-cell v-top">
+            <h1 className="me-title text-center">MATHEUS CONDINI</h1>
+            <blockquote className="me-blockquote">
+              "Show Rogerinho!"
+              </blockquote>
+          </div>
+          <div className="tbl-cell v-top"></div>
+
+        </div>
+      </div>
     </section>
   );
 }
